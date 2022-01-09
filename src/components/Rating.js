23c0429540134183d4ed7cf0ea58f1dbfb372bd2
Dessance Chandler <dchandler616@yahoo.com>
2022-01-09 00:37:18 -0500

    adding product props

---
+++ src/components/Rating.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Rating(props) { 
+    const {rating, numReviews} = props;
+    return (
+        <div className="rating"> 
+            <span> <i className={ rating >=1?"fa fa-star": rating >=0.5'fa fa-star-half-o':'a fa-star-o'}></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+        </div>
+    );
+}
